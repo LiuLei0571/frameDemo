dea84d8ee29b59e52dca70b8b933999312544203
刘磊 <liulei2@aixuedai.com>
2017-04-17 14:50:34 +0800

    搭建开发环境

---
+++ app/src/debug/java/com/lenny/framedemo/Variants.java
@@ -0,0 +1,15 @@
+package com.lenny.framedemo;
+
+/**
+ * @author Created by Lenny on 17/04/17.
+ */
+public interface Variants {
+
+    boolean LOG_D = true;
+
+    boolean LOG_DB = true;
+
+    boolean LOG_E = true;
+
+    boolean RELEASE = false; //是否正式环境
+}
---
+++ app/src/release/java/com/lenny/framedemo/Variants.java
@@ -0,0 +1,15 @@
+package com.lenny.framedemo;
+
+/**
+ * @author Created by Lenny on 17/04/17.
+ */
+public interface Variants {
+
+    boolean LOG_D = false;
+
+    boolean LOG_DB = false;
+
+    boolean LOG_E = false;
+
+    boolean RELEASE = true; //是否正式环境
+}
