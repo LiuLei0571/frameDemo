ae032711a0d78d688d4778636a920380d4f4e88c
刘磊 <liulei2@aixuedai.com>
2017-04-20 15:18:58 +0800

    简历dialog与presenter的关联

--- app/src/main/java/com/lenny/framedemo/compent/base/BaseDialog.java
+++ app/src/main/java/com/lenny/framedemo/compent/base/BaseDialog.java
@@ -3,8 +3,18 @@ package com.lenny.framedemo.compent.base;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.StyleRes;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import com.lenny.framedemo.R;
+import com.lenny.framedemo.compent.cdi.CDI;
+import com.lenny.framedemo.compent.cdi.cmp.DialogComponent;
+
+import butterknife.ButterKnife;
 
 /**
  * 用途：
@@ -12,22 +22,66 @@ import android.support.annotation.StyleRes;
  * 邮箱：649444395@qq.com
  */
 
-public abstract class BaseDialog  extends Dialog implements IView,ILoading,IDialog{
+public abstract class BaseDialog extends Dialog implements IView, ILoading, IDialog {
+    protected PresenterConnector mPresenterContorl;
+    protected DialogComponent dialogComponent;
     private BaseActivity mBaseActivity;
+    protected Bundle saveInstance;
+
     public BaseDialog(@NonNull Context context) {
-        this(context,0);
+        this(context, R.style.LoginDialogStyle);
     }
 
     public BaseDialog(@NonNull Context context, @StyleRes int themeResId) {
         super(context, themeResId);
         if (context instanceof BaseActivity) {
-            mBaseActivity= (BaseActivity) context;
+            mBaseActivity = (BaseActivity) context;
         }
     }
 
     @Override
     public void show() {
+        inits();
         super.show();
+    }
+
+    private void inits() {
+        if (mPresenterContorl == null) {
+            mPresenterContorl = new PresenterConnector();
+        }
+        if (dialogComponent == null) {
+            dialogComponent = CDI.createDialogComponent(this);
+        }
+        if (saveInstance == null) {
+            saveInstance = new Bundle();
+        }
+        doInject(this);
+        View view = getLayoutInflater().inflate(getRootView(), null);
+        beforeViewBind(view);
+        bindView(view);
+        setContentView(view);
+        afterViewBind(saveInstance);
+        mPresenterContorl.bindPresenter(saveInstance, null);
+    }
+
+    @Override
+    public void savePresenter(BasePresenter presenter) {
+        mPresenterContorl.savePresenter(presenter);
+    }
+
+    @NonNull
+    @Override
+    public Bundle onSaveInstanceState() {
+        Bundle bundle = super.onSaveInstanceState();
+        mPresenterContorl.onSaveInstanceState_(bundle);
+        return bundle;
+    }
+
+
+    @Override
+    public void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        this.saveInstance = savedInstanceState;
+        super.onRestoreInstanceState(savedInstanceState);
     }
 
     @Override
@@ -41,8 +95,54 @@ public abstract class BaseDialog  extends Dialog implements IView,ILoading,IDial
     }
 
     @Override
-    public String groupName() {
-        return mBaseActivity.getLocalClassName()+this.toString();
+    public void beforeViewBind(View rootView) {
+
     }
 
+    @Override
+    public void afterViewBind(Bundle saveInstanceState) {
+
+    }
+
+    @Override
+    public void bindView(View view) {
+        ButterKnife.bind(this, view);
+    }
+
+    @Override
+    public void unBindView() {
+        ButterKnife.unbind(this);
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void showLoading(String content) {
+
+    }
+
+    @Override
+    public void dismissLoading() {
+
+    }
+
+    @Override
+    public void startActivityForResult(Intent intent, int requestCode) {
+        FragmentActivity activity = (FragmentActivity) getActivity();
+        if (activity != null) {
+            activity.startActivityForResult(intent, requestCode);
+        }
+    }
+
+    @Override
+    public String groupName() {
+        return mBaseActivity.getLocalClassName() + this.toString();
+    }
+
+    protected abstract DialogComponent doInject(BaseDialog baseDialog);
+
+    protected abstract int getRootView();
 }
